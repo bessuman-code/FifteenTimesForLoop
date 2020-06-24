44f85f9e396085efe7156e5abf50c9007f2c7b14
U-DESKTOP-1TUJ31R\iisus <comp.structuralbiology@gmail.com>
2020-06-24 11:21:58 -0400

    For loop fifteen times

---
+++ FifteenTimesForLoop/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace FifteenTimesForLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ForFifteen();
+        }
+        public static void ForFifteen()
+        {
+            for (int i = 1; i < 16; i++)
+            {
+                Console.WriteLine($"{i} I love C#");
+            }
+        }
+    }
+}
